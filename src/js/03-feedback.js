da1c7987e249e3a331b594329b9dced371c14421
tommydrozd <tomekdro@gmail.com>
2024-04-25 14:04:17 +0200

    1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('.feedback-form input');
+const textInput = document.querySelector('.feedback-form textarea');
+
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+
+getDataFromStorage();
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+
+function onFormInput(event) {
+    const formData = form.elements;
+
+    const email = formData.email.value;
+    const message = formData.message.value;
+    const dataToSave = { email, message };
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(dataToSave));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const {elements: { email, message }}  = event.currentTarget;
+
+    if (email.value === "" || message.value === "") {
+        return window.alert("Wszystkie pola muszą zostać wypełnione!");
+    } 
+
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function getDataFromStorage() {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if (savedData) {
+        emailInput.value = savedData.email;
+        textInput.value = savedData.message;
+    }
+}
